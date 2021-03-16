c05a84d46e8f489c3d579b7acc9bcff613dfd64f
Eugenio Sales <eugenio.onboard@gmail.com>
2021-03-15 21:19:00 -0300

    format mqtt client file

--- mqtt/client/main.go
+++ mqtt/client/main.go
@@ -10,18 +10,15 @@ import (
 func main() {
 
 	broker := "localhost"
-    port := 1883
+	port := 1883
 	topic := "topic/test"
 
 	// Config
-    mqtt.NewClientOptions()
+	mqtt.NewClientOptions()
 
 	url := fmt.Sprintf("mqtt://%s:%d?topic=%s", broker, port, topic)
 
-	// url := "telegram://1630241163:AAFQxAFGFyERgAXOYKalQbxd4zKQY8J0Y_c@telegram?channels=-100145003121"
-	
 	err := shoutrrr.Send(url, "test mqtt")
 
-	fmt.Println(err);
+	fmt.Println(err)
 }
-
